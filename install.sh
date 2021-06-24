ddea63b58799b31ed7a14ce35f565eeeb96a0b2c
Agustín Ranieri <aguseranieri@gmail.com>
2021-06-24 16:58:55 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sudo rm /etc/apt/preferences.d/nosnap.pref
+sudo apt update
+sudo apt install -y python3 python3-pip python3-setuptools python3-wheel rofi zsh ripgrep ranger htop git-all make cmake valgrind libreadline-dev maven openjdk-8-jdk graphviz virtualbox testdisk snapd
+
+sudo snap install code --classic
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install spotify
+sudo snap install discord
+
+# Chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb 
+rm google-chrome-stable_current_amd64.deb 
+
+# Git credentials
+git config --global credential.helper store
+git config --global user.email "aguseranieri@gmail.com"
+git config --global user.name "Agustin Ranieri"
+
+# Bat (chequear última versión)
+wget https://github.com/sharkdp/bat/releases/download/v0.18.1/bat-musl_0.18.1_amd64.deb
+sudo dpkg -i bat-musl_0.18.1_amd64.deb 
+rm bat-musl_0.18.1_amd64.deb 
+
+# CSpec
+git clone https://github.com/mumuki/cspec.git
+make -C cspec
+sudo make install -C cspec
+rm -rf cspec
+
+# Commons
+git clone https://github.com/sisoputnfrba/so-commons-library.git 
+make -C so-commons-library debug
+sudo make install -C so-commons-library
+rm -rf so-commons-library
+
+# Oh my zsh
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+chsh -s `which zsh`
+
+# Dual boot time
+sudo timedatectl set-local-rtc 1
+
+# Reiniciar
+sudo shutdown -r 0
---
+++ install2.sh
@@ -0,0 +1,13 @@
+# Ranger
+pip install ranger-fm
+sudo ln -s $HOME/.local/bin/ranger /usr/local/bin/ranger
+sudo printf "\nalias ranger='ranger --choosedir=\$HOME/.rangerdir; LASTDIR=\$(cat \$HOME/.rangerdir); cd \"\$LASTDIR\"'\n\n" >> ~/.zshrc
+# Shortcut: gnome-terminal -e "zsh -c '. ranger;zsh'"
+
+# Node JS (chequear última versión de nvm)
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+sudo printf '\nexport NVM_DIR="$HOME/.nvm"\n[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm\n\n' >> ~/.zshrc
+nvm install --lts
+
+# Java
+sudo update-alternatives --config java # Switch to Java 8
