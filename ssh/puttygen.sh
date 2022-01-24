6a30e30bf9d1ca0dca4573cf1c03477a09963043
Agustin Ranieri <aguseranieri@gmail.com>
2022-01-24 13:09:56 -0300

    rename variables in ssh script

--- ssh/puttygen.sh
+++ ssh/puttygen.sh
@@ -3,20 +3,20 @@
 set -e
 
 NAME=${1:?}
-SSH_USER=${2:?}
-SSH_NAME=${3:?}
-SSH_HOSTNAME=${4:?}
+SSH_USERNAME=${2:?}
+SSH_SERVERNAME=${3:?}
+SSH_IPADDR=${4:?}
 
 mkdir ${NAME}
 
 # Generate ppk file
-puttygen -t rsa -b 2048 -C "${SSH_USER}@${SSH_NAME}" -o ${NAME}/${NAME}.ppk
+puttygen -t rsa -b 2048 -C "${SSH_USERNAME}@${SSH_SERVERNAME}" -o ${NAME}/${NAME}.ppk
 
 # Generate pem from ppk
 puttygen ${NAME}/${NAME}.ppk -O private-openssh -o ${NAME}/${NAME}.pem
 
 # Store public key in ssh server
-puttygen -L ${NAME}/${NAME}.ppk | xargs -i ssh ${SSH_USER}@${SSH_HOSTNAME} -f 'mkdir -p -m 700 ~/.ssh && echo {} >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'
+puttygen -L ${NAME}/${NAME}.ppk | xargs -i ssh ${SSH_USERNAME}@${SSH_IPADDR} -f 'mkdir -p -m 700 ~/.ssh && echo {} >> ~/.ssh/authorized_keys && chmod 600 ~/.ssh/authorized_keys'
 
 # Test connection
-ssh ${SSH_USER}@${SSH_HOSTNAME} -i ${NAME}/${NAME}.pem
+ssh ${SSH_USERNAME}@${SSH_IPADDR} -i ${NAME}/${NAME}.pem
