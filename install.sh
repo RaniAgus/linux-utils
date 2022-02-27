28025e1b42fa5ac6241223e064ac7046edb25b12
Agustin Ranieri <aguseranieri@gmail.com>
2022-02-26 21:19:38 -0300

    ordeno alfabéticamente

--- install.sh
+++ install.sh
@@ -28,8 +28,14 @@ git config --global credential.helper store
 git config --global user.email "aguseranieri@gmail.com"
 git config --global user.name "Agustin Ranieri"
 
-# C
-$(INSTALL) make cmake valgrind libreadline-dev entr
+# Bat
+install_dpkg https://github.com/sharkdp/bat/releases/latest/download/bat-musl_$(gh_latest_tag sharkdp/bat | sed 's/v//')_amd64.deb
+
+# Chrome
+install_dpkg https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Discord
+install_dpkg "https://discordapp.com/api/download?platform=linux&format=deb"
 
 # Docker
 $(INSTALL) apt-transport-https ca-certificates curl gnupg lsb-release
@@ -45,13 +51,9 @@ sudo usermod -aG docker $USER
 newgrp docker
 docker run hello-world
 
-# Visual Studio Code
-$(INSTALL) apt-transport-https
-wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
-sudo mv ./apt/vscode.list /etc/apt/sources.list.d/vscode.list
-$(RM) packages.microsoft.gpg
-$(INSTALL) code
+# Java
+$(INSTALL) maven openjdk-8-jdk graphviz
+sudo update-alternatives --config java
 
 # JetBrains
 if [ $MINT ]; then
@@ -61,52 +63,48 @@ fi
 sudo snap install intellij-idea-ultimate --classic
 sudo snap install rider --classic
 
+# Python
+$(INSTALL) python3 python3-pip python3-setuptools python3-wheel
+
+# Ripgrep
+install_dpkg https://github.com/BurntSushi/ripgrep/download/latest/ripgrep_$(gh_latest_tag BurntSushi/ripgrep | sed 's/v//')_amd64.deb
+
 # Spotify
 curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add -
 sudo mv ./apt/spotify.list /etc/apt/sources.list.d/spotify.list
 $(INSTALL) spotify-client
 
-# Zoom
-install_dpkg "https://zoom.us/client/latest/zoom_amd64.deb"
+# Steam
+install_dpkg https://cdn.akamai.steamstatic.com/client/installer/steam.deb
 
-# Discord
-install_dpkg "https://discordapp.com/api/download?platform=linux&format=deb"
+# UTNSO
+$(INSTALL) make cmake valgrind libreadline-dev entr
 
-# CSpec
 git clone https://github.com/mumuki/cspec.git
 make -C cspec
 sudo make install -C cspec
 $(RM) cspec
 
-# Commons
 git clone https://github.com/sisoputnfrba/so-commons-library.git
 make -C so-commons-library debug
 make install -C so-commons-library
 $(RM) so-commons-library
 
-# Python
-$(INSTALL) python3 python3-pip python3-setuptools python3-wheel
-
-# Java
-$(INSTALL) maven openjdk-8-jdk graphviz
-sudo update-alternatives --config java
-
-# Bat
-install_dpkg https://github.com/sharkdp/bat/releases/latest/download/bat-musl_$(gh_latest_tag sharkdp/bat | sed 's/v//')_amd64.deb
-
-# Chrome
-install_dpkg https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-
-# Ripgrep
-install_dpkg https://github.com/BurntSushi/ripgrep/download/latest/ripgrep_$(gh_latest_tag BurntSushi/ripgrep | sed 's/v//')_amd64.deb
-
-# Steam
-install_dpkg https://cdn.akamai.steamstatic.com/client/installer/steam.deb
-
 # VirtualBox
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 $(INSTALL) virtualbox-6.1
+
+# Visual Studio Code
+$(INSTALL) apt-transport-https
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo mv ./apt/vscode.list /etc/apt/sources.list.d/vscode.list
+$(RM) packages.microsoft.gpg
+$(INSTALL) code
+
+# Zoom
+install_dpkg "https://zoom.us/client/latest/zoom_amd64.deb"
 
 ########################################################################################################################
 
@@ -131,7 +129,6 @@ sudo ln -s "$(type -a npm | awk '{ print $NF }')" "/usr/local/bin/npm"
 pip install ranger-fm
 sudo ln -s $HOME/.local/bin/ranger /usr/local/bin/ranger
 cat ./aliases/ranger.sh >> ~/.zshrc
-# Shortcut: gnome-terminal -e "zsh -c '. ranger;zsh'"
 
 # Ruby
 $(INSTALL) libssl-dev
@@ -140,6 +137,7 @@ cat ./aliases/ruby.sh >> ~/.zshrc
 ~/.rbenv/bin/rbenv init >> ~/.zshrc
 ~/.rbenv/bin/rbenv install -l | grep -v "-" | tail -1 | xargs ~/.rbenv/bin/rbenv install
 
-sudo timedatectl set-local-rtc 1
+########################################################################################################################
 
+sudo timedatectl set-local-rtc 1
 echo "Remember to reboot! 'sudo shutdown -r 0'"
