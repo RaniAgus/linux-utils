71e6b3b6cd9ab6c87adbe62b63bc2c325ac97764
Agustin Ranieri <aguseranieri@gmail.com>
2022-06-28 16:16:35 -0300

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -1,6 +1,26 @@
 #!/bin/zsh
 
-source ./utils/functions.sh
+install() {
+  sudo apt update && sudo apt install -y "$@"
+}
+
+remove() {
+  rm -rfv "$@"
+}
+
+install_dpkg() {
+  URL=${1:?}
+  NAME=./$RANDOM.deb
+  wget -O ${NAME} "${URL}"
+  sudo apt install -y ${NAME}
+  remove ${NAME}
+}
+
+gh_latest_tag() {
+  curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
+    grep '"tag_name":' |                                            # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                    # Pluck JSON value
+}
 
 # Node
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/$(gh_latest_tag nvm-sh/nvm)/install.sh | bash
