f1ce80ff85ab0153a5abe59ab419b60f7a03f780
Agustin Ranieri <aguseranieri@gmail.com>
2022-09-19 12:58:56 -0300

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -21,7 +21,7 @@ source ~/.zshrc
 
 # Node.js
 nvm install --lts
-npm i --location=global degit yarn @angular/cli
+npm i --location=global degit yarn @angular/cli @ionic/cli
 
 # Ruby
 rbenv install $(rbenv install -l 2> /dev/null | grep -v "-" | tail -1)
