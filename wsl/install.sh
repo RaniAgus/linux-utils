da21b1fb972e0fa544a17d7797943d4b11148eda
Agustin Ranieri <aguseranieri@gmail.com>
2023-05-19 11:54:46 -0300

    Update install.sh

--- wsl/install.sh
+++ wsl/install.sh
@@ -5,3 +5,6 @@ sudo apt update && sudo apt install -y libxkbcommon-dev libgbm-dev
 
 cat ./.bashrc >> ~/.bashrc
 cat ./.bashrc >> ~/.zshrc
+
+# Git credential helper: https://github.com/git-ecosystem/git-credential-manager/blob/main/docs/wsl.md
+git config --global credential.helper "/mnt/c/Program\ Files/Git/mingw64/bin/git-credential-manager.exe"
