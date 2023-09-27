f6d53edd9bed2ac484ddf9d51a5b44b6114cd100
Agustin Ranieri <aguseranieri@gmail.com>
2023-09-26 21:06:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,6 +30,19 @@ flatpak install -y flathub \
     com.obsproject.Studio \
     org.videolan.VLC
 
+# 1Password
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+  sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/1password-archive-keyring.gpg] https://downloads.1password.com/linux/debian/$(dpkg --print-architecture) stable main" |
+  sudo tee /etc/apt/sources.list.d/1password.list
+mkdir -p /etc/debsig/policies/AC2D62742012EA22/
+curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
+  sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
+mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
+  sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
+apt_install 1password-cli
+
 # Fly.io
 curl -L https://fly.io/install.sh | sh
 
