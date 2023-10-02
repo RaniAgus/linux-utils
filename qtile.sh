7947388b3403591e4ac3814c0d1276af73797dae
Agustin Ranieri <aguseranieri@gmail.com>
2023-10-01 23:12:10 -0300

    Update qtile.sh

--- qtile.sh
+++ qtile.sh
@@ -11,3 +11,25 @@ TryExec=/usr/bin/gnome-session
 Exec=gnome-session --session=qtile
 Type=XSession
 EOF
+
+sudo tee /usr/share/gnome-session/sessions/qtile.session << EOF
+[Desktop Entry]
+Name=Qtile GNOME
+Comment=Tiling window manager
+TryExec=/usr/bin/gnome-session
+Exec=gnome-session --session=qtile
+Type=XSession
+EOF
+
+sudo tee /usr/share/applications/qtile.desktop << EOF
+[Desktop Entry]
+Type=Application
+Encoding=UTF-8
+Name=Qtile
+Exec=qtile start
+NoDisplay=true
+X-GNOME-WMName=Qtile
+X-GNOME-Autostart-Phase=WindowManager
+X-GNOME-Provides=windowmanager
+X-GNOME-Autostart-Notify=false
+EOF
