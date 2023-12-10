c75a77fcf07235b098db2ae734195c96aa558f38
Agustin Ranieri <aguseranieri@gmail.com>
2023-12-10 15:53:19 -0300

    Update noOpina.js

--- utils/noOpina.js
+++ utils/noOpina.js
@@ -1,3 +1,14 @@
+//  1: No opina
+//  2: 10%
+//  3: 20%
+//  4: 30%
+//  5: 40%
+//  6: 50%
+//  7: 60%
+//  8: 70%
+//  9: 80%
+// 10: 90%
+// 11: 100%
 [...document.getElementsByClassName('form-group')]
-  .flatMap(formGroup => [...formGroup.querySelectorAll('input[value="9"]')])
+  .flatMap(formGroup => [...formGroup.querySelectorAll('input[value="1"]')])
   .forEach(input => input.checked = true)
