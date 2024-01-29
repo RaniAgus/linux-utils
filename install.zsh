d807157969b63e69aa6e36e1746dd74cc3bd69ab
Agustin Ranieri <aguseranieri@gmail.com>
2024-01-29 00:38:28 -0300

    Update install.zsh

--- install.zsh
+++ install.zsh
@@ -29,6 +29,8 @@ go install github.com/spf13/cobra-cli@latest
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
 go install github.com/pressly/goose/v3/cmd/goose@latest
 go install github.com/a-h/templ/cmd/templ@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
 go install github.com/fullstorydev/grpcui/cmd/grpcui@latest
 go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
 
