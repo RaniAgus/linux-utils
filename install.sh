9da589fc4a0ed12d3a96122496f352b1207a1e9f
Agustin Ranieri <aguseranieri@gmail.com>
2024-04-10 14:53:10 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-apt_install() {
-  sudo apt-get update && sudo apt-get install -y "$@"
-}
-
 dpkg_install() {
   URL=${1:?}
   NAME=./$RANDOM.deb
@@ -34,7 +30,7 @@ if [ "$MINT" ]; then
 fi
 
 # Basics
-apt_install apt-transport-https curl chntpw dconf-editor drawing fd-find gnome-shell-extension-prefs hexyl htop hyperfine jq pass p7zip-full rename ripgrep silversearcher-ag snapd software-properties-common testdisk tree usb-creator-gtk wget zip
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl chntpw dconf-editor drawing fd-find gnome-shell-extension-prefs hexyl htop hyperfine jq pass p7zip-full rename ripgrep silversearcher-ag snapd software-properties-common testdisk tree usb-creator-gtk wget zip
 
 if [ "$NOSNAP" ]; then
   flatpak install -y flathub \
@@ -58,7 +54,7 @@ curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
   sudo gpg --yes --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-apt_install 1password 1password-cli
+sudo apt-get update && sudo apt-get install -y 1password 1password-cli
 
 mkdir -p ~/.ssh
 chmod 600 ~/.ssh
@@ -80,7 +76,7 @@ bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/
 # Git
 sudo add-apt-repository -y ppa:git-core/ppa
 
-apt_install git-all
+sudo apt-get update && sudo apt-get install -y git-all
 # dpkg_install "https://github.com/git-ecosystem/git-credential-manager/releases/latest/download/gcm-linux_amd64.$(gh_latest_tag git-ecosystem/git-credential-manager).deb"
 # git-credential-manager configure
 # gpg --generate-key
@@ -104,7 +100,7 @@ git config --global core.protectNTFS false
 curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
 sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
 echo "deb [arch=${ARCHITECTURE:?} signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
-apt_install gh
+sudo apt-get update && sudo apt-get install -y gh
 
 # Bat
 dpkg_install "https://github.com/sharkdp/bat/releases/latest/download/bat-musl_$(gh_latest_tag sharkdp/bat)_amd64.deb"
@@ -121,17 +117,17 @@ else
 fi
 
 # Docker
-apt_install ca-certificates gnupg lsb-release
+sudo apt-get update && sudo apt-get install -y ca-certificates gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=${ARCHITECTURE:?} signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu ${LSB_RELEASE:?} stable" \
   | sudo tee /etc/apt/sources.list.d/docker.list
-apt_install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo usermod -aG docker "$USER"
 echo "run 'docker run hello-world' to test docker installation"
 newgrp docker
 
 # DotNET
-apt_install dotnet6
+sudo apt-get update && sudo apt-get install -y dotnet6
 
 # Fly.io
 curl -L https://fly.io/install.sh | sh
@@ -151,7 +147,7 @@ git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
 # Java
 wget -O- https://apt.corretto.aws/corretto.key | sudo apt-key add -
 sudo add-apt-repository -y 'deb https://apt.corretto.aws stable main'
-apt_install maven java-17-amazon-corretto-jdk java-11-amazon-corretto-jdk java-1.8.0-amazon-corretto-jdk graphviz
+sudo apt-get update && sudo apt-get install -y maven java-17-amazon-corretto-jdk java-11-amazon-corretto-jdk java-1.8.0-amazon-corretto-jdk graphviz
 # sudo update-alternatives --config java
 curl -s "https://get.sdkman.io" | bash
 # shellcheck disable=SC1091
@@ -168,7 +164,7 @@ wget -qO- "${TBA_LINK:?}" | sudo tar xvzC /opt
 # Ngrok
 curl -fsSL "https://ngrok-agent.s3.amazonaws.com/ngrok.asc" | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null
 echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list
-apt_install ngrok
+sudo apt-get update && sudo apt-get install -y ngrok
 
 # Posman
 if [ "$NOSNAP" ]; then
@@ -178,16 +174,16 @@ else
 fi
 
 # Protocol buffers
-apt_install protobuf-compiler
+sudo apt-get update && sudo apt-get install -y protobuf-compiler
 
 # Python
-apt_install python3 python3-pip python3-setuptools python3-wheel
+sudo apt-get update && sudo apt-get install -y python3 python3-pip python3-setuptools python3-wheel
 
 ## Ranger
 pip install ranger-fm
 
 ## yt-dlp
-apt_install ffmpeg
+sudo apt-get update && sudo apt-get install -y ffmpeg
 pip install yt-dlp
 
 ## yq
@@ -209,7 +205,7 @@ deb [arch=${ARCHITECTURE:?} signed-by=/etc/apt/keyrings/llvm-snapshot.gpg] http:
 deb-src [arch=${ARCHITECTURE:?} signed-by=/etc/apt/keyrings/llvm-snapshot.gpg] http://apt.llvm.org/${LSB_RELEASE:?}/ llvm-toolchain-${LSB_RELEASE:?} main
 EOF
 
-apt_install make clang-tidy clang-format cmake entr libreadline-dev libcunit1 libcunit1-doc libcunit1-dev meson ninja-build remake shellcheck valgrind
+sudo apt-get update && sudo apt-get install -y make clang-tidy clang-format cmake entr libreadline-dev libcunit1 libcunit1-doc libcunit1-dev meson ninja-build remake shellcheck valgrind
 
 git clone https://github.com/mumuki/cspec.git
 make -C cspec
@@ -233,7 +229,7 @@ sudo mv tailwindcss-linux-x64 /usr/local/bin/tailwindcss
 echo "deb [arch=${ARCHITECTURE:?} signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian ${LSB_RELEASE:?} contrib" \
   | sudo tee /etc/apt/sources.list.d/virtualbox.list
 curl -fsSL https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes -o /usr/share/keyrings/oracle-virtualbox-2016.gpg
-apt_install virtualbox-7.0
+sudo apt-get update && sudo apt-get install -y virtualbox-7.0
 
 # Zoom
 flatpak install -y flathub us.zoom.Zoom
@@ -242,7 +238,7 @@ sed -i 's/enableMiniWindow=.*/enableMiniWindow=false/' ~/.var/app/us.zoom.Zoom/c
 ########################################################################################################################
 
 # Oh My Zsh
-apt_install zsh
+sudo apt-get update && sudo apt-get install -y zsh
 sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 # shellcheck disable=SC2006,SC2046
 chsh -s `which zsh`
