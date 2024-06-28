5f2d77fdfbd1be9240aee0ba5b5afe6557a692d7
Agustin Ranieri <aguseranieri@gmail.com>
2024-06-28 11:00:20 -0300

    Update Microsoft.Powershell_profile.ps1

--- windows/Microsoft.Powershell_profile.ps1
+++ windows/Microsoft.Powershell_profile.ps1
@@ -2,6 +2,6 @@ oh-my-posh init pwsh --config C:\Users\agustin.ranieri\.config\oh-my-posh\zen.to
 
 if ($host.Name -eq 'ConsoleHost')
 {
-    function ls_git { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -hF $args }
+    function ls_git { & 'C:\Program Files\Git\usr\bin\ls' --color=auto -w 0 -hF $args }
     Set-Alias -Name ls -Value ls_git -Option Private
 }
