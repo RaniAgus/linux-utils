4cd9b92f91c077addea65c482d211ded4bc1af7b
Agustin Ranieri <aguseranieri@gmail.com>
2025-08-03 17:58:39 -0300

    feat: fedora

---
+++ fedora.sh
@@ -0,0 +1,152 @@
+#!/bin/bash -x
+
+set -e
+
+gh_latest_tag() {
+  curl -fsSL "https://api.github.com/repos/$1/releases/latest" | jq -r '.tag_name' | sed 's/v//g'
+}
+
+flatpak install -y flathub \
+  com.discordapp.Discord \
+  com.google.Chrome \
+  com.getpostman.Postman \
+  org.kde.kdenlive \
+  net.pcsx2.PCSX2 \
+  com.github.jeromerobert.pdfarranger \
+  com.obsproject.Studio \
+  org.videolan.VLC \
+  us.zoom.Zoom
+
+sed -i 's/enableMiniWindow=.*/enableMiniWindow=false/' ~/.var/app/us.zoom.Zoom/config/zoomus.conf
+
+sudo dnf update -y
+
+sudo dnf -y install dnf-plugins-core ffmpeg python3 python3-pip python3-setuptools python3-wheel jq
+
+# Code
+
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" \
+  | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+sudo dnf install -y code
+
+# 1password
+
+sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
+
+echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" \
+  | sudo tee /etc/yum.repos.d/1password.repo > /dev/null
+
+sudo dnf install -y 1password
+
+mkdir -p ~/.config/autostart
+cat <<EOF | nano ~/.config/autostart/1password.desktop
+[Desktop Entry]
+Type=Application
+Name=1Password
+Exec=1password
+X-GNOME-Autostart-enabled=true
+EOF
+
+# JetBrains Mono
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
+# Git
+
+git config --global init.defaultBranch main
+git config --global user.email "aguseranieri@gmail.com"
+git config --global user.name "Agustin Ranieri"
+git config --global credential.username "RaniAgus"
+git config --global gpg.format ssh
+
+# C/C++
+
+sudo dnf install -y gcc gcc-c++ gdb make entr readline-devel shellcheck valgrind clang-tidy clang-format
+
+git clone https://github.com/mumuki/cspec.git
+make -C cspec
+sudo make install -C cspec
+rm -rfv cspec
+
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+make -C so-commons-library debug install
+rm -rfv so-commons-library
+
+sudo mkdir /usr/local/include/doctest
+curl -fsSL "https://raw.githubusercontent.com/doctest/doctest/v2.4.12/doctest/doctest.h" \
+  | sudo tee /usr/local/include/doctest/doctest.h > /dev/null
+
+# Docker
+
+sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl enable --now docker
+sudo docker run hello-world
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+echo "Run 'docker run hello-world' to verify Docker installation."
+newgrp docker
+
+# Go
+
+wget -qO- "https://go.dev/dl/$(curl -fsSL 'https://golang.org/VERSION?m=text' | head -n1).linux-amd64.tar.gz"   | sudo tar xvzC /usr/local
+git config --global url."git@github.com:".insteadOf "https://github.com/"
+git config --global url."git@bitbucket.org:".insteadOf "https://bitbucket.org/"
+
+# Java
+
+cat <<EOF > /etc/yum.repos.d/adoptium.repo
+[Adoptium]
+name=Adoptium
+baseurl=https://packages.adoptium.net/artifactory/rpm/fedora/\$releasever/\$basearch
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.adoptium.net/artifactory/api/gpg/key/public
+EOF
+
+sudo dnf install temurin-21-jdk
+
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install gradle
+sdk install quarkus
+
+# JetBrains
+
+TBA_LINK=$(curl -fsSL "https://data.services.jetbrains.com/products/releases?code=TBA&latest=true&type=release" | jq -r '.TBA[0].downloads.linux.link')
+wget -qO- "${TBA_LINK:?}" | sudo tar xvzC /opt
+/opt/jetbrains-toolbox-*/bin/jetbrains-toolbox
+
+# PSEInt
+
+curl -L "https://downloads.sourceforge.net/project/pseint/20230517/pseint-l64-20230517.tgz" | sudo tar xvzC /opt
+
+# Rust
+
+curl https://sh.rustup.rs -sSf | sh
+
+# yt-dlp
+
+pip install yt-dlp
+pip install yq
+
+# VirtualBox
+
+sudo rpm --import https://www.virtualbox.org/download/oracle_vbox_2016.asc
+
+curl -fsSL https://download.virtualbox.org/virtualbox/rpm/fedora/virtualbox.repo \
+  | sudo tee /etc/yum.repos.d/virtualbox.repo > /dev/null
+
+sudo dnf install -y VirtualBox-7.1
+
+# ZSH
+
+sudo dnf install -y zsh
+
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+chsh -s $(which zsh)
---
+++ fedora.zsh
@@ -0,0 +1,61 @@
+#!/bin/zsh
+
+gh_latest_tag() {
+  curl -fsSL "https://api.github.com/repos/$1/releases/latest" | jq -r '.tag_name' | sed 's/v//g'
+}
+
+# Oh My Posh
+mkdir -p ~/.oh-my-posh/bin ~/.config/oh-my-posh
+curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.oh-my-posh/bin
+cat ./dotfiles/.config/oh-my-posh/zen.toml > ~/.config/oh-my-posh/zen.toml
+
+# Nerd Font
+wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v$(gh_latest_tag ryanoasis/nerd-fonts)/JetBrainsMono.zip"
+unzip JetBrainsMono.zip -d ~/.local/share/fonts/fonts/ttf
+rm -v JetBrainsMono.zip
+
+# bun
+curl -fsSL https://bun.sh/install | bash
+
+# nvm
+wget -qO- "https://raw.githubusercontent.com/nvm-sh/nvm/v$(gh_latest_tag nvm-sh/nvm)/install.sh" | bash
+
+# rbenv
+sudo dnf install openssl-devel zlib-devel libyaml-devel libffi-devel bison g++
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+
+# Rust
+cargo install zoxide --locked
+
+# Install aliases
+cat ./dotfiles/.zshrc >> ~/.zshrc
+# shellcheck disable=SC1090,SC3046
+source ~/.zshrc
+
+# Go
+go install github.com/go-task/task/v3/cmd/task@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+go install github.com/fullstorydev/grpcui/cmd/grpcui@latest
+go install github.com/fullstorydev/grpcurl/cmd/grpcurl@latest
+
+# Node.js
+nvm install --lts
+npm i --location=global npm@latest @angular/cli degit http-server tldr typescript yarn
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+curl -fsSL https://dprint.dev/install.sh | sh
+
+# Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
+
+# Ruby
+rbenv install -l 2> /dev/null | grep '^3' | tail -1 | xargs rbenv install
+rbenv versions | xargs rbenv global
+gem install pry bundler rspec colorize rails jekyll
+
+########################################################################################################################
+
+# Dual boot timezone fix
+# sudo timedatectl set-local-rtc 1
+echo "Remember to reboot! 'sudo shutdown -r 0'"
