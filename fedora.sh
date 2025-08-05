42ae2acc1c3cd8168dde40331342757f63358825
Agustin Ranieri <aguseranieri@gmail.com>
2025-08-05 19:17:02 -0300

    Update fedora.sh

--- fedora.sh
+++ fedora.sh
@@ -91,15 +91,17 @@ curl -fsSL "https://raw.githubusercontent.com/doctest/doctest/v2.4.12/doctest/do
 
 # Docker
 
-sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo systemctl enable --now docker
-sudo docker run hello-world
-sudo groupadd docker
-sudo usermod -aG docker $USER
+#sudo dnf-3 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+#sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#sudo systemctl enable --now docker
+#sudo docker run hello-world
+#sudo groupadd docker
+#sudo usermod -aG docker $USER
 
-echo "Run 'docker run hello-world' to verify Docker installation."
-newgrp docker
+sudo dnf install podman-compose
+
+# echo "Run 'docker run hello-world' to verify Docker installation."
+# newgrp docker
 
 # Go
 
